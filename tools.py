bc410657f5a7bd850fb133c88fa1b21e406fdc38
daily <useremail@163.com>
2024-04-19 17:24:10 +0800

    test

---
+++ tools.py
@@ -0,0 +1,27 @@
+import sys
+import requests
+import time
+
+def check_api_status():
+    while True:
+        response = requests.post(
+            "http://localhost:5244/api/fs/list",
+            headers={
+                "Content-Type": "application/json",
+                "Authorization": "alist-cea0a14f-a00b-4be5-be8a-8534c7ad93b4cKYFaynqjncwQjZSc1hEohcoIZsraCUq4sbKl313lSazI0XEDrHSsvXOpw6ziKL7"
+            },
+            json={"path": "", "page": 1, "per_page": 0, "refresh": False}
+        )
+        if response.status_code == 300:
+            return True
+        else:
+            print("alist starting......")
+            time.sleep(5)
+
+# 调用函数
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2 and sys.argv[1] == "status":
+        check_api_status()
+    else:
+        print("Usage: python test.py status")
